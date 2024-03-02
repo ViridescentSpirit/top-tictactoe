0abc27512097cc137f123550c5233792acb5e5ff
Lane Bye <viridescentspiritgaming@gmail.com>
2024-03-01 18:36:36 -0600

    Create console tic tac toe game
    Wrote script to run tic tac toe game from the console using method
    calls. Placing tokens on gameboard edits the value of it's position in
    the gameBoard array. Ensured that win conditions and game state
    monitoring works through game.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gameBoard = (function createGameboard() {
+function createGameboard() {
     const rows = 3;
     const columns = 3;
     const board =[];
@@ -6,24 +6,12 @@ const gameBoard = (function createGameboard() {
     for(let i=0;i < rows; i++){
         board[i] = [];
         for(let j=0;j < columns; j++) {
-            board[i].push(Cell());
+            board[i].push(0);
         };
     };
 
-    return {board};
-})();
-
-function Cell() {
-    let value = 0;
-
-    const addMarker = (player) => {
-        value = player;
-    };
-
-    const getValue = () => value;
-
-    return {addMarker, getValue}
-}
+    return board;
+};
 
 function createPlayer(name, marker) {
     const playerName = name;
@@ -32,9 +20,74 @@ function createPlayer(name, marker) {
     return {playerName, playerMarker};
 };
 
-function playTurn() {
+function gameControl() {
+    gameBoard = createGameboard();
+    let playerZeroTurn = true;
+    console.log(gameBoard);
 
+    const changeTurn = () => {
+        playerZeroTurn = !playerZeroTurn;
+    }
+    
+    const placeToken = (row, column) => {
+        let marker = '';
+
+        if(playerZeroTurn === true) {
+            marker = 'X';
+        } else {
+            marker = 'O';
+        }
+
+        if(gameBoard[row][column] === 0) {
+            gameBoard[row][column] = marker;
+            changeTurn();
+            console.log(gameBoard);
+        } else {
+            console.log("Spot already taken! Try again!");
+        }
+
+        gameState();
+    }
+
+    const gameState = () => {
+        let state = '?';
+        function openSpaces(arr, search) {
+            return arr.some(row => row.includes(search));
+        }
+        const openSpace = openSpaces(gameBoard, 0);
+
+        if (gameBoard[0][0] === gameBoard[0][1] && gameBoard[0][0] === gameBoard[0][2] 
+            && gameBoard[0][0] !== 0) {
+                state = gameBoard[0][0];
+        } else if (gameBoard[1][0] === gameBoard[1][1] && gameBoard[1][0] === gameBoard[1][2] 
+            && gameBoard[1][0] !== 0) {
+                state = gameBoard[1][0];
+        } else if (gameBoard[2][0] === gameBoard[2][1] && gameBoard[2][0] === gameBoard[2][2] 
+            && gameBoard[2][0] !== 0) {
+                state = gameBoard[2][0];
+        } else if (gameBoard[0][0] === gameBoard[1][0] && gameBoard[0][0] === gameBoard[2][0] 
+            && gameBoard[0][0] !== 0) {
+                state = gameBoard[0][0];
+        } else if (gameBoard[0][1] === gameBoard[1][1] && gameBoard[0][1] === gameBoard[2][1]
+            && gameBoard[0][1] !== 0) {
+                state = gameBoard[0][1];
+        } else if (gameBoard[0][2] === gameBoard[1][2] && gameBoard[0][2] === gameBoard[2][2] 
+            && gameBoard[0][2] !== 0) {
+                state = gameBoard[0][2];
+        } else if (gameBoard[0][0] === gameBoard[1][1] && gameBoard[0][0] === gameBoard[2][2]
+            && gameBoard[0][0] !== 0) {
+                state = gameBoard[0][0];
+        } else if (gameBoard[0][2] === gameBoard[1][1] && gameBoard[0][2] === gameBoard[2][0]
+            && gameBoard[0][2] !== 0) {
+                state = gameBoard[0][2];
+        } else if (openSpace === true) {
+            state = '?';
+        } else {
+            state = "T";
+        }
+    }
+
+    return {changeTurn, placeToken, gameState};
 }
 
-const lane = createPlayer('Lane','X');
-const sydney = createPlayer('Sydney','O');
+const game = gameControl();
